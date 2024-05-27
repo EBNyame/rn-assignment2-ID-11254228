fb183d427375ca2580eeb4c5c04443e02387df25
Joshua <amjoshua59@gmail.com>
2024-05-27 16:17:08 +0000

    increase the font size

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My Name is Exodus</Text>
+      <Text style={{ fontSize: 24 }}>My Name is Exodus</Text>
       <StatusBar style="auto" />
     </View>
   );
